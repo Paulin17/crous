47b7b5f1df56e205ffffe47f56ae584b7c8c1fec
Paulin17 <paulin.doyon15@gmail.com>
2024-09-09 21:03:55 +0200

    v2.0

--- script_dev.sh
+++ script_dev.sh
@@ -7,9 +7,9 @@ curl \
         -H "Title: Demarrage script" \
         -H "Tags: heavy_check_mark," \
         -d "En attente de repas" \
-        ntfy.sh/dev_repas_crous 1> /dev/null 2>/dev/null #envoie une notification de démarrage
+        ntfy.sh/debug_repas_crous 1> /dev/null 2>/dev/null #envoie une notification de démarrage
 
-url=https://crousandgo.crous-poitiers.fr/larochelle/categorie-produit/sites-la-rochelle/ #URL Crous
+url_index=https://crousandgo.crous-poitiers.fr/larochelle/categorie-produit/sites-la-rochelle/ #URL Crous
 
 declare -rA asso_mois=(
     ['Janvier']='01'
@@ -29,88 +29,73 @@ declare -rA asso_mois=(
 #Reinitialisation de toutes les variables
 reset(){ 
     echo $(date '+%y-%m-%d %H:%M:%S')" WARNING Reset script (variables,folder,pause)"
-    rm -R tmp/ 2>/dev/null
-    mkdir tmp/
     echo "0" > pause
-    nb_repas=0
-    nbrepas_tmp=0
     unset dico_jour
     declare -g -A dico_jour
 }
 
-download_index(){ #Telecharge la page principale de vente et la met dans $code_html
-    code_html=$(curl -s "$url")
-    echo $(date '+%y-%m-%d %H:%M:%S')" NOTICE Téléchargement de l'accueil (index)"
-}
-
-download_secondaires(){
-    #Extraction des url des repas:
-    #              affiche le code   Récupere les href        cherche la chaine \/       cut le href    enleve les doublons
-    repas_links=$(echo $code_html | grep -oE 'href="([^"#]+)"'|grep 'repas-la-rochelle'| cut -d'"' -f2|uniq)
-    
-    #Telecharge les pages des repas
-    for link in $repas_links; do
-        code_temp=$(curl -s "$link") #Telecharge la page
-
-        traitement_temp=$(echo $code_temp |grep -oP '<h1.*?>(.*?)<\/h1>' | sed -e 's/<[^>]*>//g'|tr ' ' '_') #Récupere uniquement le contenue de H1 en remplacant les espaces par des tirret du bas.
-        if [[ -v dico_jour["$traitement_temp"] ]]; then
-            :
-        else
-            jour=$(echo $traitement_temp | cut -d '_' -f 5 | sed 's/^./\U&/')
-            date=$(echo $traitement_temp |cut -d '_' -f 6)
-            mois=$(echo $traitement_temp |cut -d '_' -f 7)
-            year=$(echo $traitement_temp |cut -d '_' -f 8)
-
-            current_date=$(date +%d-%m-%Y)
-
-            if [[ "$current_date" < "$year-${asso_mois[$mois]}-$date" ]]; then
-                #La date n'est pas encore passé
-
-                if grep -q "végétarien"<<< $1 ;then 
-                    type='Végétarien'
-                else
-                    type='Normal'
-                fi
-
-                dico_jour["$traitement_temp"]=$(echo "$jour $date $mois $year-$type"|tr ' ' '_') #Enregistre l'entré dans un dictionaire
-                echo $code_temp > "tmp/$traitement_temp.html" #Enregistre dans le fichier
-            else
-                #La date est déjà passé on s'en fou
-                pass
-            fi
-        fi
-        echo $(date '+%y-%m-%d %H:%M:%S')" DEBUG Telechargement de "$traitement_temp
-    done
-}
-
-
-send_notif(){ #Prend un parametre un nombre de repas et un tableau contenant les jour et envoie une notification
+send_notif(){ #Prend un parametre un tableau contenant les jours et envoie une notification
     var=""
-    for jour in "${@:2}"; do
+    for jour in "${@}"; do
         var+="$jour\n"
     done
     curl \
-    -H "Title: "$1" Nouveau repas Disponibles" \
+    -H "Title: "$#" Nouveau repas Disponibles" \
     -H "Tags: warning," \
     -d "$(echo -e "$var"|tr '_' ' ')" \
-    ntfy.sh/dev_repas_crous 1> /dev/null 2>/dev/null #envoie une requette de notification
+    ntfy.sh/repas_crous 1> /dev/null 2>/dev/null #envoie une requette de notification
 }
 
 wait_samedi(){ #Attend le prochain samedi a 00h01
     next_saturday=$(date -d 'next Saturday 12:01' +'%s')
     current_time=$(date +'%s')
     time_to_wait=$((next_saturday - current_time))
-    curl -H "Title: Attente jusqu'au prochain Samedi" -d "temp: $time_to_wait" ntfy.sh/dev_repas_crous 1> /dev/null 2>/dev/null #envoie une notification de démarrage
+    curl -H "Title: Attente jusqu'au prochain Samedi" -d "Temp: $time_to_wait" ntfy.sh/debug_repas_crous 1> /dev/null 2>/dev/null #envoie une notification de démarrage
     echo $(date '+%y-%m-%d %H:%M:%S')" WARNING  Attente jusqu'au prochain samedi a 12h01, temp: $time_to_wait"
     sleep $time_to_wait
-    curl -H "Title: Redémarage Script" -d "En attente de repas" ntfy.sh/dev_repas_crous 1> /dev/null 2>/dev/null #envoie une notification de démarrage
+    curl -H "Title: Redémarage Script" -d "En attente de repas" ntfy.sh/debug_repas_crous 1> /dev/null 2>/dev/null #envoie une notification de démarrage
 }
 
 reset
 while true ; do
-    download_index
-    download_secondaires
+    #Telecharge la page principale de vente et la met dans $code_html
+    code_html=$(curl -s "$url_index")
+    echo $(date '+%y-%m-%d %H:%M:%S')" NOTICE Téléchargement de l'accueil (index)"
 
+    #Extraction des url des repas:
+    #              affiche le code   Récupere les href        cherche la chaine \/       cut le href    enleve les doublons
+    repas_links=$(echo $code_html | grep -oE 'href="([^"#]+)"'|grep 'repas-la-rochelle'| cut -d'"' -f2|uniq)
+    
+    #Telecharge les pages des repas
+    for link in $repas_links; do
+        code_temp=$(curl -s "$link") #Telecharge la page du repas
+
+        #Récupere uniquement le contenue de H1 en remplacant les espaces par des tirret du bas.
+        traitement_temp=$(echo $code_temp |grep -oP '<h1.*?>(.*?)<\/h1>' | sed -e 's/<[^>]*>//g'|tr ' ' '_')
+        echo $(date '+%y-%m-%d %H:%M:%S')" DEBUG Telechargement de "$traitement_temp
+
+        if [[ -v dico_jour["$traitement_temp"] ]]; then
+            : #Si le jour est déjà present dans le dictionaire, alors on ne fait rien.
+        else
+            #Récuperation des information spécifique du repas
+            jour=$(echo $traitement_temp | cut -d '_' -f 5 | sed 's/^./\U&/')
+            date=$(echo $traitement_temp |cut -d '_' -f 6)
+            mois=$(echo $traitement_temp |cut -d '_' -f 7)
+            year=$(echo $traitement_temp |cut -d '_' -f 8)
+        
+            current_date=$(date +%d-%m-%Y)
+            #On vérifie si le repas n'est pas dans le passé...
+            if [[ "$current_date" < "$date-${asso_mois[$mois]}-$year" ]]; then
+                #La date n'est pas encore passé
+
+                type=$(grep -q "végétarien" <<< $traitement_temp && echo 'Végétarien' || echo 'Normal')
+
+                dico_jour["$traitement_temp"]=$(echo "$jour $date $mois $year $type"|tr ' ' '_') #Enregistre l'entré dans un dictionaire
+            fi
+        fi
+        
+    done
+    
     unset notif
     declare -a notif
 
@@ -123,13 +108,18 @@ while true ; do
     done
 
     if [[ ${#notif[@]} -ne 0 ]]; then
-        send_notif ${#notif[@]} ${notif[@]}
+        echo $(date '+%y-%m-%d %H:%M:%S')' INFO Préparation notif : nb-repas '${#notif[@]}
+        echo $(date '+%y-%m-%d %H:%M:%S')' INFO |-> repas : '"${notif[@]}"
+        send_notif ${notif[@]}
+        echo $(date '+%y-%m-%d %H:%M:%S')' NOTICE Notification envoyée !'
+        
     fi
     
-    if [ "$nb_repas" -gt 9 ] || [ "$(cat pause)" -eq 1 ]; then
+    if [ ${#dico_jour[@]} -gt 9 ] || [ "$(cat pause)" -eq 1 ]; then
         wait_samedi
+        reset
     else
         echo $(date '+%y-%m-%d %H:%M:%S')" NOTICE En attente de nouveau repas, prochaine tentative dans 60s"
-        sleep 1
+        sleep 60
     fi
 done
