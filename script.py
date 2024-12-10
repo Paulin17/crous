064c843d9d338ca26758073d3dd148587ad92b21
LucasStagnette <103002404+LucasStagnette@users.noreply.github.com>
2024-12-10 04:05:10 +0100

    start & few tests
    l3-6 initial commit
    other : test

--- script.py
+++ script.py
@@ -1,5 +1,16 @@
 import requests,datetime,sys,os
 from fonctions import *
+import time
+from bs4 import BeautifulSoup
+url = "https://crousandgo.crous-poitiers.fr/larochelle/categorie-produit/sites-la-rochelle/"
+code = html_pars(url)
+jours = list(find(code))
+for repas in jours:
+    a = (str(repas).split('<h2 class="woocommerce-loop-product__title">')[1])
+    b = a.split('</h2></a>')[0]
+    print(b)
+
+
 
 LOG_FILE = f"LOG/{datetime.datetime.now().strftime('%Y-%m-%d_%Hh-%Mm-%Ss')}.log"
 os.makedirs("LOG", exist_ok=True)
